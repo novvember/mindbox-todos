12fbb41ad1bc609c27f729a08e40b06490a6762b
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-10-30 21:57:05 +0300

    feat: add layout for add input

--- src/components/Add/Add.tsx
+++ src/components/Add/Add.tsx
@@ -1,9 +1,35 @@
+import classNames from 'classnames';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import './Add.css';
 
 function Add() {
+  const [value, setValue] = useState('');
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
-    <form action="" className="add">
-      <input className='add__input' type="text" placeholder='What needs to be done?'></input>
+    <form action="" className="add" onSubmit={handleSubmit}>
+      <input
+        className="add__input"
+        type="text"
+        placeholder="What needs to be done?"
+        value={value}
+        onChange={handleChange}
+      />
+      <button
+        className={classNames('add__save-button', {
+          'add__save-button_active': value,
+        })}
+        type="submit"
+      >
+        Add
+      </button>
     </form>
   );
 }
