17531d2629f95dc8b7750dd6f0e9b2ec444ac3c1
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-11-01 01:53:32 +0300

    style: improve text styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,9 +3,8 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__text">Test task for mindbox</p>
       <p className="footer__text">
-        Created by{' '}
+        Test task for mindbox. Created by{' '}
         <a href="https://github.com/novvember/mindbox-todos">novvember</a> in
         10/2022
       </p>
