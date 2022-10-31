ed813358fabd3496f15fe8b4310155788a684eec
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-10-31 20:28:03 +0300

    feat: add layout for filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,7 +1,41 @@
 import './Filter.css';
 
 function Filter() {
-  return <div>Filter</div>;
+  return (
+    <section className="filter">
+      <span className="filter__status">2 items left</span>
+      <form action="" className="filter__groups">
+        <label className="filter__radio-container">
+          <input
+            type="radio"
+            className="filter__radio-input"
+            id="all"
+            name="group"
+          />
+          <span className="filter__radio-label">All</span>
+        </label>
+        <label className="filter__radio-container">
+          <input
+            type="radio"
+            className="filter__radio-input"
+            id="active"
+            name="group"
+          />
+          <span className="filter__radio-label">Active</span>
+        </label>
+        <label className="filter__radio-container">
+          <input
+            type="radio"
+            className="filter__radio-input"
+            id="completed"
+            name="group"
+          />
+          <span className="filter__radio-label">Completed</span>
+        </label>
+      </form>
+      <button className="filter__delete-button">Clear completed</button>
+    </section>
+  );
 }
 
 export default Filter;
