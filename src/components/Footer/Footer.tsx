49e7b03a1bf4880fc87c35f0c2a19bac011c37ad
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-10-31 20:32:12 +0300

    style: style text in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,11 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">Test task for mindbox</p>
-      <p className="footer__text">Created by novvember in 10/2022</p>
+      <p className="footer__text">
+        Created by{' '}
+        <a href="https://github.com/novvember/mindbox-todos">novvember</a> in
+        10/2022
+      </p>
     </footer>
   );
 }
